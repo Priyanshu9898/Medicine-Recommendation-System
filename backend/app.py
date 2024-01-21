f199c3893a7655330f0913665093fec62295ec17
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-01-21 18:43:28 +0530

    debug is false in backend

--- backend/app.py
+++ backend/app.py
@@ -52,4 +52,4 @@ def index():
     return "<h1>Medicine Recommendation System Backend</h1> <br /> Go to Swagger Docs: <a href='/swagger'>Link</a>"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
