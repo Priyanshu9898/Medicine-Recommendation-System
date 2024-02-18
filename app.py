8d8182525d1243b86fad45fc7f955ba79a25d75d
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-02-18 23:51:39 +0530

    frontend urls added

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 
 
 # Define allowed origins for CORS
-allowed_origins = [ "https://meta-doctor.vercel.app", "https://meta-doctor.vercel.app/",  "https://metadoctorhelper.vercel.app", "http://localhost:3000",
-                   "http://127.0.0.1:3000", "https://metadoctorhelper.vercel.app", "https://meta-doctor.vercel.app", "https://meta-doctor-nwxeblfpx-priyanshumalaviya9210-gmailcom.vercel.app", "https://meta-doctor-git-main-priyanshumalaviya9210-gmailcom.vercel.app"]
+allowed_origins = ["https://metadoctor.vercel.app/", "https://meta-doctor.vercel.app", "https://meta-doctor.vercel.app/", "https://metadoctor-git-main-priyanshumalaviya9210-gmailcom.vercel.app/",  "https://metadoctorhelper.vercel.app", "http://localhost:3000",
+                   "http://127.0.0.1:3000", "https://metadoctorhelper.vercel.app", "https://metadoctor-priyanshumalaviya9210-gmailcom.vercel.app/", "https://meta-doctor.vercel.app", "https://meta-doctor-nwxeblfpx-priyanshumalaviya9210-gmailcom.vercel.app", "https://meta-doctor-git-main-priyanshumalaviya9210-gmailcom.vercel.app"]
 
 CORS(app)
 
